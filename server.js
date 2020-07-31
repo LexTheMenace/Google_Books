07dc7d060c28481877b31778af51f4dfeea16b82
Lex <lexthemenace@gmail.com>
2020-07-31 07:40:50 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body Parser MiddleWare
+app.use(bodyParser.jdon());
+
