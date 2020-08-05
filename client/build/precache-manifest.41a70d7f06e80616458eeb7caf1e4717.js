fd03d28baafcbf8ee402d8da07968d3f3eaa4b36
Lex <lexthemenace@gmail.com>
2020-08-05 01:50:16 -0500

    build folder

---
+++ client/build/precache-manifest.41a70d7f06e80616458eeb7caf1e4717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3860fbc679d872bc5baadf6a876927a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3983cb697167dad9b802",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a33b0d9a514315115335",
+    "url": "/static/css/main.0b958981.chunk.css"
+  },
+  {
+    "revision": "3983cb697167dad9b802",
+    "url": "/static/js/2.b98e5127.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b98e5127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33b0d9a514315115335",
+    "url": "/static/js/main.96bfef33.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
